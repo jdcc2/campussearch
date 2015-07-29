c3f635c0c6dd9cf84e63d8a63b4b1e6a99b52db1
JH Kuipers <janharm@appgenerator.nl>
2015-07-30 00:33:40 +0200

    Add basic GUI (without background) and keyboard navigation

--- campussearch.py
+++ campussearch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 __author__ = 'jd'
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf, Gdk
 import requests
 
 """
@@ -9,62 +9,191 @@ TODO
 - Catch keyboard events
 - Handle arrow down on searchbox
 - Check if keynav-failed event gets emitted on arrow up on upper boundary of list, if yes catch that event
+- rescale background image to size of window
+- use css to make the background of the layout containers transparent
 
 """
 
 
-class CampusSearch(Gtk.Window):
+class CampusSearch():
     def __init__(self):
-        Gtk.Window.__init__(self, title="CampusSearch")
-        self.set_border_width(10)
 
-        #overlay so we can put widgets ontop of background image
+        #Create main window
+        self.window = Gtk.Window(title="CampusSearch", type=Gtk.WindowType.TOPLEVEL)
+        self.window.set_border_width(10)
+
+        #Create background image overlay so we can put widgets on top of background image
         self.overlay = Gtk.Overlay()
-        #self.add(self.overlay)
-        self.background = Gtk.Image.new_from_file('/home/jd/Pictures/Wallpapers/techcon.jpg')
-        #self.overlay.add(self.background)
+        #self.window.add(self.overlay)
 
 
-        self.result_list = Gtk.ListBox()
-        #self.searchbox = SearchBox()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file('/home/jd/Pictures/LA1.jpg')
+        #Adjust the size of the image to match the screen
 
+        #Get the size of the monitor (because window.get_size() fails)
+        s = self.window.get_screen()
+        m = s.get_monitor_at_window(s.get_active_window())
+        monitor = s.get_monitor_geometry(m)
+
+        print("Monitor =>  Height: %s, Width: %s" % (monitor.height, monitor.width))
+
+        scaled_pixbuf = pixbuf.scale_simple(monitor.width - 50, monitor.height- 50, GdkPixbuf.InterpType.BILINEAR)
+
+
+
+        self.background = Gtk.Image.new_from_pixbuf(scaled_pixbuf)
+        self.overlay.add(self.background)
+
+
+
+
+
+        #Create top-level layout container
         self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.vbox.pack_start(self.result_list, False, False, 0)
-        self.vbox.pack_start(Gtk.Entry(), False, False, 0)
-        self.add(self.vbox)
-        #self.overlay.add_overlay(self.vbox)
+        self.vbox.set_name("vbox")
 
-        self.fullscreen()
+        self.overlay.add_overlay(self.vbox)
+
+
+        #Terminate app on window close
+        self.window.connect("delete-event", Gtk.main_quit)
+
+
+        #Apply CSS
+        self.applyCSS()
+
+        self.addSearchBox()
+        self.addResultList()
+
+        for i in range(20):
+            self.addResultItem(i ,i*10,i*100)
+
+
+
+        self.connectSignals()
+
+        ########WARNING###########
+        #The following sequence of statements is the only way in which a background image gets drawn in a fullscreen window
+
+
+        self.window.add(self.overlay)
+        #Show the window
+        self.window.fullscreen()
+        self.window.show_all()
+
+
+        #Add the background
+
+
+
+        #Redraw to actually show the background
+        self.window.show_all()
+
+####### GUI setup functions
+
+        def applyCSS(self):
+
+        style_provider = Gtk.CssProvider()
+
+        css = """
+
+
+            GtkContainer {
+                background-color : transparent;
+
+
+            }
+
+            GtkBox {
+                padding-left : 100px;
+                padding-right : 100px;
+            }
+
+            GtkLabel {
+
+                padding-left : 100px;
+            }
+
+
+        """
+
+
+
+        style_provider.load_from_data(bytes(css.encode()))
+
+        Gtk.StyleContext.add_provider_for_screen(
+            Gdk.Screen.get_default(),
+            style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+
+    def addResultList(self):
+        self.scrollwindow = Gtk.ScrolledWindow()
+        self.result_list = Gtk.ListBox()
+        self.scrollwindow.add(self.result_list)
+        #widget, expand, fill, padding
+        self.vbox.pack_start(self.scrollwindow, True, True, 30)
+
+    def resetResults(self):
+        self.vbox.remove(self.scrollwindow)
+        self.addResultList()
+
+
+
+    def addSearchBox(self):
+        self.searchbox = Gtk.Entry()
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        box.pack_start(self.searchbox, True, True, 50)
+        self.vbox.pack_start(box, False, False, 30)
 
     def addResultItem(self, title, smb_url, share_name):
         row = Gtk.ListBoxRow()
-        item = ResultItem(title, smb_url, share_name)
+        item = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        item.add(Gtk.Label(title, xalign=0))
+        item.add(Gtk.Label(smb_url, xalign=0))
+        item.add(Gtk.Label(share_name, xalign=0))
         row.add(item)
         self.result_list.add(row)
 
 
-    def resetResults(self):
-        self.vbox.remove(self.result_list)
-        self.result_list = Gtk.ListBox()
-        self.vbox.add(self.result_list)
-
-#class SearchBox(Gtk.Entry):
-#
- #   def __init__(self, defaulttext=""):
-  #      self.set_text(defaulttext)
 
 
 
-class ResultItem(Gtk.Box):
-    def __init__(self, title, smb_url, share_name):
-        self.add(Gtk.Label(title))
+    def connectSignals(self):
+        self.result_list.connect("keynav-failed", self.listNavFailed)
+        self.result_list.connect("key_press_event", self.resultKeyPress)
+        self.searchbox.connect("key_press_event", self.searchKeyPress)
+
+
+########Signal Handlers
+
+    def searchKeyPress(self, widget, key_event):
+        if key_event.keyval == 65293:
+            print("SearchBox: enter pressed")
+            searchstring = widget.get_text()
+            print("Searchstring: " + searchstring)
+            widget.set_text("")
+            self.doSearch(searchstring)
+
+    def resultKeyPress(self, widget, key_event):
         pass
 
+    def listNavFailed(self, widget, direction):
+        if direction == Gtk.DirectionType.UP:
+            self.searchbox.grab_focus()
+
+########Functions
+
+    def doSearch(self, searchstring):
+        pass
+
+    def doAddResults(self, results):
+
+
+
+
+
+
 if __name__ == "__main__":
-    win = CampusSearch()
-
-    win.connect("delete-event", Gtk.main_quit)
-    win.show_all()
-
-
+    cs = CampusSearch()
     Gtk.main()
